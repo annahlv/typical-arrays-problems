5d273ec3b97e56cf7ade9ae0077b11eefe3951eb
annahlv <annahlv.work@gmail.com>
2021-03-10 12:15:40 +0200

    feat: add functionality

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-
-exports.min = function min (array) {
-  return 0;
+function compareNumeric(a, b) {
+  if (a > b) return 1;
+  if (a == b) return 0;
+  if (a < b) return -1;
 }
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.min = function min(array) {
+  if (!Array.isArray(array) || !array.length) return 0;
+  let a = array.sort(compareNumeric);
+  return a[0];
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.max = function max(array) {
+  if (!Array.isArray(array) || !array.length) return 0;
+  let a = array.sort(compareNumeric).reverse();
+  return a[0];
+};
+
+exports.avg = function avg(array) {
+  if (!Array.isArray(array) || !array.length) return 0;
+  let total = array.reduce(function (a, b) {
+    return a + b;
+  });
+  return total / array.length;
+};
